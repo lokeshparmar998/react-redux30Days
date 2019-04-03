f04147f01617d0c53e3a1d2aef875851fbc12a3c
lokesh parmar <lokesh.parmar998@gmail.com>
2019-04-04 01:59:59 +0530

    Props and list of props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Sub from'./subComp';
 class App extends Component {
+  state={
+    details:[
+      {name: 'Lokesh',age:20,place:'bhiwani',id:1},
+      {name: 'Meenakshi',age:20,place:'gurgaon',id:2},
+      {name: 'Rohit',age:21,place:'jhajjar',id:3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello world
         </h1>
-        <Sub/>
+          {/* <Sub name="Lokesh" age="20" place="gurgaon"/>  */} 
+          <Sub detail={this.state.details}/>
       </div>
     );
   }
--- myapp/src/subComp.js
+++ myapp/src/subComp.js
@@ -2,11 +2,28 @@ import React, {Component} from 'react';
 
 class Sub extends Component{
     render(){
+        const {detail }= this.props; // note: use these {} for props otherwise error
+        const detailList =detail.map(make1 => {
+            return (
+                <div className="details" key={make1.id}>
+                <h1>Name:{make1.name}</h1> 
+                <h1>Age:{make1.age}</h1> 
+                <h1>place:{make1.place}</h1> 
+                </div>
+            )
+        })
+        {/*console.log(this.props);
+        const {name,age,place}=this.props;  destruct props*/}
         return(
             <div className="sub">
-                <h1>hello i am in sub component</h1>
+              { /* <h1>Name:</h1> <p>{name}</p> <br/> 
+                <h1>Age:</h1> <p>{age}</p> <br/> 
+                <h1>place:</h1> <p>{this.props.place}</p> <br/> */}
+                {
+                    detailList
+                }
             </div> 
         );
-    }
+    } 
 }
 export default Sub;
