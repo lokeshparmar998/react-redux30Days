17208db7310c51fd610d032a8f4afddd52464fee
lokesh parmar <lokesh.parmar998@gmail.com>
2019-04-04 00:44:32 +0530

    some more notes

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Sub from'./subComp';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world
+        </h1>
+        <Sub/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ myapp/src/subComp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Sub extends Component{
+    render(){
+        return(
+            <div className="sub">
+                <h1>hello i am in sub component</h1>
+            </div> 
+        );
+    }
+}
+export default Sub;
