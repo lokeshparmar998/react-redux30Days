0b4b91d086f92fe8546159461b88dc4a78c9a316
lokesh parmar <lokesh.parmar998@gmail.com>
2019-04-05 01:38:58 +0530

    statless component with condition operator

--- myapp/src/subComp.js
+++ myapp/src/subComp.js
@@ -4,13 +4,20 @@ const Sub = (props)=> {
     // we can de structure the props by const Sub = ({details}) =>{  then we wont need the line below
        const {detail }= props; //we dont need this as we are passing props and not using the instance.
         const detailList =detail.map(make1 => {
-            return (
-                <div className="details" key={make1.id}>
-                <h1>Name:{make1.name}</h1> 
-                <h1>Age:{make1.age}</h1> 
-                <h1>place:{make1.place}</h1> 
-                </div>
-            )
+            if(make1.age  >20)
+            {
+                return (
+                    <div className="details" key={make1.id}>
+                    <h1>Name:{make1.name}</h1> 
+                    <h1>Age:{make1.age}</h1> 
+                    <h1>place:{make1.place}</h1> 
+                    </div>
+                )
+            }
+            else{
+                return null;
+            }
+            
         })
         return(
             <div className="sub">
